09ccb08b46698d218f73ae1a20ec18d08bbb5373
ashish yadav <ashishashen@gmail.com>
2019-02-20 03:48:58 -0800

    steps challange

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n,row = 0,stair ='') {
+    // for (var i = 0;i<n;i++){
+    //     var toprint = '';
+    //     for( var j=0; j<n;j++){
+    //         if(j<=i){
+    //             toprint = '#'+toprint;
+    //         }else{
+    //             toprint =  toprint+' ';
+    //         }
+    //         }
+    //     console.log(toprint);
+    // }
 
+    //recursion solution
+    
+    if (n===row){
+        return;
+    }
+
+    if(n === stair.length){
+        console.log(stair);
+        return steps(n,row+1);
+    }
+    
+    if(stair.length <= row){
+        stair+= '#';
+    }else{
+        stair+= ' ';
+    }
+    steps(n,row,stair);
+
+}
+
+//steps(3);
 module.exports = steps;
